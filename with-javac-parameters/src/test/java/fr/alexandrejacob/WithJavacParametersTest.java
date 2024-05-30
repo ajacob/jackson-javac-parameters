67dffe089c55078bb025873e7eb6b736e2e8b62c
Alexandre Jacob <alexandref.jacob@gmail.com>
2024-05-30 19:44:56 +0200

    Add serialization tests

--- with-javac-parameters/src/test/java/fr/alexandrejacob/WithJavacParametersTest.java
+++ with-javac-parameters/src/test/java/fr/alexandrejacob/WithJavacParametersTest.java
@@ -3,13 +3,14 @@ package fr.alexandrejacob;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WithJavacParametersTest {
 
     @Test
-    void test() throws JsonProcessingException {
+    void test_deserialization() throws JsonProcessingException {
         var jsonPayload = """
                 {
                     "key1": "val1",
@@ -24,5 +25,22 @@ public class WithJavacParametersTest {
 
         Assertions.assertEquals(payload.getKey1(), "val1");
         Assertions.assertEquals(payload.getKey2(), "val2");
+    }
+
+    @Test
+    void test_serialization() throws JsonProcessingException {
+        Payload payload = new Payload("val1", "val2");
+
+        String jsonPayload = new ObjectMapper()
+                .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS) // deterministic output
+                .enable(SerializationFeature.INDENT_OUTPUT)
+                .findAndRegisterModules()
+                .writeValueAsString(payload);
+
+        Assertions.assertEquals("""
+                {
+                  "key2" : "val2",
+                  "key1" : "val1"
+                }""", jsonPayload);
     }
 }
